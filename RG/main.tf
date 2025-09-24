8495ab7f29ff29b263bc492f0ac196d0ee2de1b2
Sunil Kumar <erkumarsunil5786@gmail.com>
2025-09-24 07:51:37 +0530

    RG Added

--- RG/main.tf
+++ RG/main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-24th_sept"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg3" {
+  name     = "rg-24th_sept2"
+  location = "West Europe"
+}
