e553052e1dba47aeb4f083936c2bd6df5381265a
erkumarsunil5786 <erkumarsunil5786@gmail.com>
2025-09-24 08:13:24 +0530

    Update main.tf

--- RG/main.tf
+++ RG/main.tf
@@ -12,3 +12,7 @@ resource "azurerm_resource_group" "rg2" {
   name     = "rg-24th_sept2"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "rg4" {
+  name     = "rg-24th_sept4"
+  location = "West Europe"
+}
