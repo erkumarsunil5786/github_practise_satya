6700b41c2ff55075296c550daa5a49d336044c07
Sunil Kumar <erkumarsunil5786@gmail.com>
2025-09-24 08:12:05 +0530

    RG1

--- RG/main.tf
+++ RG/main.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "rg1" {
   name     = "rg-24th_sept1"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg2" {
+  name     = "rg-24th_sept2"
+  location = "West Europe"
+}
